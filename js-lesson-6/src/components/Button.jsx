384cea31cf311f8cc287630ba60643351989e739
Mariia Paraketsova <paraketsova@gmail.com>
2020-12-08 10:56:17 +0100

    Add for button counter click to add size button

--- js-lesson-6/src/components/Button.jsx
+++ js-lesson-6/src/components/Button.jsx
@@ -1,7 +1,22 @@
+import React, {useState} from 'react';
 import {ButtonStyled} from './ButtonStyled'
 
+
+
 export default function Button(props) {
+
+  const [counter, setCounter] = useState(0);
+
+function handleOnClick() {
+  setCounter(counter + 1);
+}
+
   return (
-    <ButtonStyled sweden={props.sweden}>My button</ButtonStyled>
+    <ButtonStyled 
+    onClick={handleOnClick}
+    sweden={props.sweden}
+    counter={counter}
+    >
+      My button</ButtonStyled>
   )
 }
--- js-lesson-6/src/components/ButtonStyled.jsx
+++ js-lesson-6/src/components/ButtonStyled.jsx
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
+
 const ButtonStyled = styled.button`
   padding: 1rem 1.6rem;
+  font-size: ${props => 10+props.counter}px;
   background: ${props => props.sweden ? 'blue' : 'black'};
   color: ${props => props.sweden ? 'yellow' : 'white'};
 
